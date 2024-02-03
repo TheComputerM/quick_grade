5d8b0bf529368fbc53d43e53ffbf63d61e433b4f
TheComputerM <mudit.somani00@gmail.com>
2024-02-04 02:38:23 +0530

    feat: add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,98 @@ fn main() {
     dioxus_desktop::launch(App);
 }
 
+struct CourseEntry {
+    credit: u16,
+    grade: u16,
+}
+
+struct Courses(Vec<CourseEntry>);
+
 // define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
+    use_shared_state_provider(cx, || Courses(Vec::new()));
+
     cx.render(rsx! {
-        div {
-            "Hello, world!"
+        Form {}
+        CourseList {}
+        FinalSG {}
+    })
+}
+
+fn Form(cx: Scope) -> Element {
+    let courses = use_shared_state::<Courses>(cx).unwrap();
+    cx.render(rsx! {
+        form {
+            onsubmit: move |event| {
+                let credit = event.data.values["credit"][0].parse::<u16>().unwrap();
+                let grade = event.data.values["grade"][0].parse::<u16>().unwrap();
+                let entry = CourseEntry {
+                    credit: credit,
+                    grade: grade
+                };
+                courses.write().0.push(entry);
+            },
+            div {
+                label {
+                    r#for: "credit",
+                    "Select course credits"
+                }
+                select {
+                    name: "credit",
+                    for i in 1..6 {
+                        option {
+                            value: i,
+                            format!("{i} credit course")
+                        }
+                    }
+                },
+            }
+            div {
+                label {
+                    r#for: "grade",
+                    "Select grade"
+                }
+                select {
+                    name: "grade",
+                    option { value: 10, "A" },
+                    option { value: 9, "A-" },
+                    option { value: 8, "B" },
+                    option { value: 7, "B-" },
+                    option { value: 6, "C" },
+                    option { value: 5, "C-" },
+                },
+            }
+            input { r#type: "submit", },
+        }
+    })
+}
+
+fn CourseList(cx: Scope) -> Element {
+    let courses = use_shared_state::<Courses>(cx).unwrap();
+
+    cx.render(rsx! {
+        ul {
+            for course in courses.read().0.iter() {
+                li {
+                    format!("Credit: {},  Grade: {}", course.credit, course.grade)
+                }
+            }
+        }
+    })
+}
+
+fn FinalSG(cx: Scope) -> Element {
+    let courses = &use_shared_state::<Courses>(cx).unwrap().read().0;
+    let total_credits = f64::from(courses.iter().fold(0, |acc, course| acc + course.credit));
+    let aquired_credits = f64::from(
+        courses
+            .iter()
+            .fold(0, |acc, course| acc + course.credit * course.grade),
+    );
+    let sg = aquired_credits / total_credits;
+    cx.render(rsx! {
+        p {
+            sg.to_string()
         }
     })
 }
