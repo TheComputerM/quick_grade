c6e853668527be3cd6e49929acb17e1eb984e426
Srivardhan <srivardhansharma2809@gmail.com>
2024-02-21 00:50:26 +0530

    Added functionality to name the course and a reset button

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 #![allow(non_snake_case)]
-// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+
 use dioxus::prelude::*;
 
+
 fn main() {
-    // launch the dioxus app in a webview
     dioxus_desktop::launch(App);
 }
 
 struct CourseEntry {
+    name: String,
     credit: u16,
     grade: u16,
 }
 
 struct Courses(Vec<CourseEntry>);
 
-// define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     use_shared_state_provider(cx, || Courses(Vec::new()));
 
@@ -27,17 +27,30 @@ fn App(cx: Scope) -> Element {
 
 fn Form(cx: Scope) -> Element {
     let courses = use_shared_state::<Courses>(cx).unwrap();
+	
     cx.render(rsx! {
         form {
             onsubmit: move |event| {
+                let name = event.data.values["name"][0].clone();
                 let credit = event.data.values["credit"][0].parse::<u16>().unwrap();
                 let grade = event.data.values["grade"][0].parse::<u16>().unwrap();
                 let entry = CourseEntry {
+                    name: name,
                     credit: credit,
                     grade: grade
                 };
                 courses.write().0.push(entry);
             },
+            div {
+                label {
+                    r#for: "name",
+                    "Enter course name"
+                }
+                input {
+                    r#type: "text",
+                    name: "name",
+                },
+            }
             div {
                 label {
                     r#for: "credit",
@@ -66,9 +79,13 @@ fn Form(cx: Scope) -> Element {
                     option { value: 7, "B-" },
                     option { value: 6, "C" },
                     option { value: 5, "C-" },
+                    option { value: 4, "D" },
+                    option { value: 2, "E" },
                 },
             }
             input { r#type: "submit", },
+           
+            input { r#type: "reset", value: "Reset"},
         }
     })
 }
@@ -80,7 +97,7 @@ fn CourseList(cx: Scope) -> Element {
         ul {
             for course in courses.read().0.iter() {
                 li {
-                    format!("Credit: {},  Grade: {}", course.credit, course.grade)
+                    format!("Name: {}, Credit: {}, Grade: {}", course.name, course.credit, course.grade)
                 }
             }
         }
@@ -90,19 +107,16 @@ fn CourseList(cx: Scope) -> Element {
 fn FinalSG(cx: Scope) -> Element {
     let courses = &use_shared_state::<Courses>(cx).unwrap().read().0;
     let total_credits = courses.iter().fold(0, |acc, course| acc + course.credit);
-    let aquired_credits = courses
-        .iter()
-        .fold(0, |acc, course| acc + course.credit * course.grade);
-
-    let sg = f64::from(aquired_credits)
-        / if total_credits == 0 {
-            1.0
-        } else {
-            f64::from(total_credits)
-        };
+    let aquired_credits = courses.iter().fold(0, |acc, course| acc + course.credit * course.grade);
+    let sg = f64::from(aquired_credits) / if total_credits == 0 {
+        1.0
+    } else {
+        f64::from(total_credits)
+    };
     cx.render(rsx! {
         p {
             sg.to_string()
         }
     })
 }
+
